171d2c17b8eb8a62d2c527afaaa17d0c8e02af99
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-06-27 13:12:15 -0300

    Updated ValuesController

--- EFCore.WebApi/EFCore.WebAPI/Controller/ValuesController.cs
+++ EFCore.WebApi/EFCore.WebAPI/Controller/ValuesController.cs
@@ -1,25 +1,72 @@
+
+
+public readonly HeroiContext _context {get; set;}
+
+public ValuesController(HeroiContext context)
+{
+    _context = context;
+}
+
+[HttpGet]
 public ActionResult<IEnumerable<string>> Get()
 {
     return new string[] { "value1", "value2"};
 }
 
-// GET api/values/5
-public ActionResult Get(int id)
+// Select
+[HttpGet("filtro/nome")]
+public ActionResult Get(string nome)
 {
-    var heroi = new Heroi{};
-    using(var contexto = new HeroiContext())
-    {
-        contexto.Herois.Add(heroi);
-        //contexto.Add(heroi);
-        contexto.SaveChanges();
-    }
+    var list Heroi = _context.Herois
+                    .Where( h => h.Nome.Contains(nome))
+                    .ToList();
+   //var listHeroi = _context.Herois.ToList();
+   (from heroi in _context.Herois
+   where heroi.Nome.Contains(nome) 
+   select heroi).ToList();
+   return Ok(listHeroi);
+
+   foreach(var item in listHeroi)
+   {
+       realizaCalculo();
+       criaArquivos();
+       salvaRelatorio();
+   }
+}
+
+//Update
+[HttpGet("{nameHero}")]
+// GET api/values/5
+public ActionResult Get(int nameHero)
+{
+    var listHeroi = _context.Herois
+                    .Where( h => h.id ==1)
+                    FirstOrDefault();
+    
+    listHeroi.Nome = "Homem Aranha";
 
     return OK();
 }
 
 // POST api/values
-[HttpPost]
-public void Post([FromBody] string value)
+[HttpGet]("AddRange")]
+public ActionResult GetAddRange()
 {
+    _context.AddRange(
+        new Heroi { Nome = "Capitão América"},
+        new Heroi { Nome = "Doutor Estranho"},
+        new Heroi { Nome = "Homem Aranha"},
+        new Heroi { Nome = ""}
+    )
+}
 
+[httpDelete("{id}")]
+public void Delete(int id)
+{
+    var heroi = _context.Herois
+                    .Where(x => x.Id == id)
+                    .Single();
+
+    _context.Herois.Remove(heroi);
+    _context.SaveChanges();
 }
