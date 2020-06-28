bd9eb6006d4280637b4359f893339f4e41e145b0
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-06-28 08:27:18 -0300

    update HeroiController

--- EFCore.WebApi/EFCore.WebAPI/Controller/HeroiController.cs
+++ EFCore.WebApi/EFCore.WebAPI/Controller/HeroiController.cs
@@ -10,6 +10,36 @@ namespace EFCore.WebAPI.Controllers
             _context;
         }
 
+        [HttpPost]
+        public ActionResult Post(HeroiController value)
+        {
+            try
+            {
+            
+               var heroi = new HeroiController
+               {
+                   Id = id,
+                   Nome = "Homem de ferro",
+                   Armas = new List<Arma>
+                   {
+                       new Arma {Nome = "Mac 3"},
+                       new Arma {Nome = "Mac 5"}
+                   }
+               };
+
+               _context.Herois.Add(heroi);
+               _context.SaveChanges();
+
+               return OK(BAZINGA);
+            }
+
+
+            catch (Exception ex)
+            {
+                return BadRequest
+            }
+        }
+
         [HttpGet]
         public ActionResult Get()
         {
