fee710bd23644fb245689ecc13c826b4150b5dbd
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-06-29 08:57:11 -0300

    update IEFCoreRepository]

--- EFCore.WebApi/EFCore.WebAPI/Interfaces/IEFCoreRepository.cs
+++ EFCore.WebApi/EFCore.WebAPI/Interfaces/IEFCoreRepository.cs
@@ -6,6 +6,9 @@ namespace EFCore.Repo
 {
     public interface IEFCoreRepository
     {
-        
+       void Add<T>(T entity) where T : class;
+       void Update<T>(T entity) where T : class; 
+       void Delete<T>(T entity) where T : class;
+       Task<bool> SaveChangeAsync();
     }
 }
