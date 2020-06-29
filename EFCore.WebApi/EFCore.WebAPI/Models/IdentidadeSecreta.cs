1247c1cf2620658ef33ec0c0af872e5846b736d0
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-06-29 08:07:34 -0300

    minor change

--- EFCore.WebApi/EFCore.WebAPI/Models/IdentidadeSecreta.cs
+++ EFCore.WebApi/EFCore.WebAPI/Models/IdentidadeSecreta.cs
@@ -9,7 +9,7 @@ namespace EFCore.WebAPI.Models
     public class IdentidadeSecreta
     {
         public int Id { get; set;}
-        public int NomeReal {get; set;}
+        public string NomeReal {get; set;}
         public int HeroiId{get; set;}
         public Heroi Heroi{get; set;}
     }
